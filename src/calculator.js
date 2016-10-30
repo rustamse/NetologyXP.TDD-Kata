68f0acad5265dbfe05aca51d712b6155fa6e6834
rustamse <rustamse@gmail.com>
2016-10-30 16:02:03 +0500

    Первая поптыка, хотя увлекся и затратил минут 35

--- src/calculator.js
+++ src/calculator.js
@@ -4,4 +4,46 @@ export class Calculator {
     constructor() {
     }
 
+    add(inputString) {
+        if (inputString.length == 0)
+            return 0;
+
+        let customDelimiter = this._findCustomDelimiter(inputString);
+        let lineDelimiter = '\n';
+        let stringDelimiter = customDelimiter == null ? ',' : customDelimiter;
+        let inputStringWithoutMarker = customDelimiter == null ? inputString : inputString.substring(3);
+
+        let splitter = new RegExp(lineDelimiter + '|' + stringDelimiter);
+
+        let numbers = inputStringWithoutMarker.split(splitter);
+
+        var sum = this._calcSum(numbers);
+        return sum;
+    }
+
+    _findCustomDelimiter(numbersString) {
+        let customDelimiterIndex = numbersString.indexOf('//');
+        if (customDelimiterIndex == 0) {
+            return numbersString[2];
+        }
+        else {
+            return null;
+        }
+    }
+
+    _calcSum(numbers) {
+        let sum = 0;
+        let errorMsg = '';
+        for (let i = 0; i < numbers.length; i++) {
+            var numberVal = parseInt(numbers[i]);
+            if (numberVal < 0)
+                errorMsg += 'negatives not allowed ' + numberVal;
+            sum += numberVal;
+        }
+
+        if (errorMsg.length > 0) {
+            throw new Error(errorMsg);
+        }
+        return sum;
+    }
 }
--- test/calculatorTests.js
+++ test/calculatorTests.js
@@ -1,17 +1,95 @@
 import assert from 'assert'
 import sinon from 'sinon'
-import {PizzaOrder} from '../src/calculator'
+import {Calculator} from '../src/calculator'
 
 // http://osherove.com/tdd-kata-1/
 suite('TDD Kata Calculator tests', function () {
+    let calculator = new Calculator();
 
-    suite('suite', function () {
+    suite('when input sring is empty', function () {
+        test('returns 0', function () {
+            let expected = 0;
 
-        test('test', function () {
+            let res = calculator.add("0");
 
-            assert.equal(true, true);
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is 1', function () {
+        test('returns 1', function () {
+            let expected = 1;
+
+            let res = calculator.add("1");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is 1,2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add("1,2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is 1,2,7', function () {
+        test('returns 10', function () {
+            let expected = 10;
+
+            let res = calculator.add("1,2,7");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is 1\n2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add("1\n2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is 1,2\n7', function () {
+        test('returns 10', function () {
+            let expected = 10;
+
+            let res = calculator.add("1,2\n7");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring is //;1;2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add("//;1;2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('when input sring contains negative numbers', function () {
+        suite('when input sring is -1', function () {
+            test('exception throws negatives not allowed', function () {
+
+                assert.throws(()=> calculator.add('-1'), /negatives not allowed/);
+            });
         });
 
+        suite('when input string is -5,-6', function () {
+            test('exception throws negatives not allowed for each number', function () {
+
+                assert.throws(()=> calculator.add('-5,-6'), /negatives not allowed -5negatives not allowed -6/);
+            });
+        });
     });
 
 });
