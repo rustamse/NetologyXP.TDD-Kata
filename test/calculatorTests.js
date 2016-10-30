40ace13b3b33fe0538bff3b9968265864ccb9e80
rustamse <rustamse@gmail.com>
2016-10-30 14:50:46 +0500

    Подготовил проект для TDD-Kata

---
+++ test/calculatorTests.js
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import sinon from 'sinon'
+import {PizzaOrder} from '../src/calculator'
+
+// http://osherove.com/tdd-kata-1/
+suite('TDD Kata Calculator tests', function () {
+
+    suite('suite', function () {
+
+        test('test', function () {
+
+            assert.equal(true, true);
+        });
+
+    });
+
+});
