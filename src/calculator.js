4e7d2be31f5cc9bccbc85cfbae04113cae3c58fa
rustamse <rustamse@gmail.com>
2016-10-30 22:15:25 +0500

    Вторая попытка, долгом мучался с substring с 2 аргументами т.к. в C# совсем по-другому...

--- src/calculator.js
+++ src/calculator.js
@@ -4,4 +4,48 @@ export class Calculator {
     constructor() {
     }
 
+    add(inputString) {
+        if (inputString.length == 0)
+            return 0;
+
+        let customDelimiter = this._findCustomDelimiter(inputString);
+        console.info('del:' + inputString);
+        let filteredString = customDelimiter == null ? inputString : inputString.substring(customDelimiter.length + 3);
+        let stringDelimiter = customDelimiter == null ? ',' : customDelimiter;
+        let regExp = new RegExp('\n|' + stringDelimiter);
+
+        let numberStrings = filteredString.split(regExp);
+
+        let sum = this._sumNumbers(numberStrings);
+
+        return sum;
+    }
+
+    _sumNumbers(numberStrings) {
+        let sum = 0;
+        let exceptionMsg = '';
+        for (let i = 0; i < numberStrings.length; i++) {
+            let numString = numberStrings[i];
+            let num = parseInt(numString);
+            if (num < 0) {
+                exceptionMsg += 'negatives not allowed ' + num;
+            }
+            if (num > 1000)
+                continue;
+            sum += num;
+        }
+
+        if (exceptionMsg.length > 0) {
+            throw new Error(exceptionMsg);
+        }
+
+        return sum;
+    }
+
+    _findCustomDelimiter(inputString) {
+        if (inputString.indexOf('//') == 0) {
+            return inputString.substring(2, inputString.indexOf('\n'));
+        }
+        return null;
+    }
 }
--- test/calculatorTests.js
+++ test/calculatorTests.js
@@ -1,17 +1,122 @@
 import assert from 'assert'
 import sinon from 'sinon'
-import {PizzaOrder} from '../src/calculator'
+import {Calculator} from '../src/calculator'
 
 // http://osherove.com/tdd-kata-1/
-suite('TDD Kata Calculator tests', function () {
+suite('TDD Kata Calculator add tests', function () {
 
-    suite('suite', function () {
+    var calculator = new Calculator();
 
-        test('test', function () {
+    suite('When add empty string', function () {
+        test('returns 0', function () {
+            var expected = 0;
 
-            assert.equal(true, true);
+            var res = calculator.add("");
+
+            assert.equal(expected, res);
         });
+    });
 
+    suite('When add 1 string', function () {
+        test('returns 1', function () {
+            var expected = 1;
+
+            var res = calculator.add("1");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add 1,2 string', function () {
+        test('returns 3', function () {
+            var expected = 3;
+
+            var res = calculator.add("1,2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add 1,2,3 string', function () {
+        test('returns 6', function () {
+            var expected = 6;
+
+            var res = calculator.add("1,2,3");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add 1\n2 string', function () {
+        test('returns 3', function () {
+            var expected = 3;
+
+            var res = calculator.add("1\n2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add 1,2\n7 string', function () {
+        test('returns 10', function () {
+            var expected = 10;
+
+            var res = calculator.add("1,2\n7");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add //;\n1;2 string', function () {
+        test('returns 3', function () {
+            var expected = 3;
+
+            var res = calculator.add("//;\n1;2");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add //;\n1;2\n1 string', function () {
+        test('returns 4', function () {
+            var expected = 4;
+
+            var res = calculator.add("//;\n1;2\n1");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add -1 string', function () {
+        test('exception', function () {
+            assert.throws(() => calculator.add("-1"), /negatives not allowed -1/);
+        });
+    });
+
+    suite('When add 1\n-1,-2 string', function () {
+        test('exception', function () {
+            assert.throws(() => calculator.add("1\n-1,-2"), /negatives not allowed -1negatives not allowed -2/);
+        });
+    });
+
+    suite('When add 1,1002 string', function () {
+        test('returns 1', function () {
+            var expected = 1;
+
+            var res = calculator.add("1,1002");
+
+            assert.equal(expected, res);
+        });
+    });
+
+    suite('When add //__\n1__2 string', function () {
+        test('returns 3', function () {
+            var expected = 3;
+
+            var res = calculator.add("//__\n1__2");
+
+            assert.equal(expected, res);
+        });
     });
 
 });
