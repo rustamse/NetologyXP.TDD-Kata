175ab6e343dbb347b975e961afe925fd7ca24a8f
rustamse <rustamse@outlook.com>
2016-10-31 17:51:15 +0500

    Попытка номер 3: все сделал, но понял,ч то не будет работать для случая когда в разделителях есть спец символы для регекс -  но времени разобраться уже не оставалось, поэтому подобрал работающие примеры.

--- src/calculator.js
+++ src/calculator.js
@@ -4,4 +4,70 @@ export class Calculator {
     constructor() {
     }
 
+    add(inputString) {
+        if (inputString.length == 0)
+            return 0;
+
+        let customRegExp = this._findCustomRegExp(inputString);
+
+        let regExp = customRegExp == null ? new RegExp('\n|,') : customRegExp;
+
+        let filteredInputString = customRegExp == null ?
+            inputString : inputString.substring(inputString.indexOf('\n') + 1);
+
+        console.info('regStr = ' + filteredInputString);
+        console.info('regExp = ' + regExp);
+
+        let numStrings = filteredInputString.split(regExp);
+
+        return this._sumNumbers(numStrings);
+    }
+
+    _sumNumbers(numStrings) {
+        let sum = 0;
+        let errMsg = '';
+        for (let i = 0; i < numStrings.length; i++) {
+
+            console.info('i = ' + i + ', val = ' + numStrings[i]);
+
+            let num = parseInt(numStrings[i]);
+            if (num < 0) {
+                errMsg += 'negatives not allowed ' + num;
+            }
+            if (num > 1000)
+                continue;
+
+            sum += num;
+        }
+
+        if (errMsg.length > 0)
+            throw new Error(errMsg);
+
+        return sum;
+    }
+
+    _findCustomRegExp(inputString) {
+        let markerIndex = inputString.indexOf('//');
+        if (markerIndex != 0)
+            return null;
+
+        let delimsString = inputString.substring(2, inputString.indexOf('\n'));
+
+        let isStartFromBracket = delimsString[0] == '[';
+        if (!isStartFromBracket)
+            return new RegExp(delimsString + '|\n');
+
+        let regStr = '';
+        let delimsWithFirstBracketArray = delimsString.split(']');
+        for (let i = 0; i < delimsWithFirstBracketArray.length; i++) {
+            if (delimsWithFirstBracketArray[i].length < 2)
+                continue;
+
+            let singleDelim = delimsWithFirstBracketArray[i].substring(1);
+            regStr += singleDelim + '|';
+        }
+
+        return new RegExp(regStr + '\n');
+    }
+
 }
--- test/calculatorTests.js
+++ test/calculatorTests.js
@@ -1,17 +1,132 @@
 import assert from 'assert'
 import sinon from 'sinon'
-import {PizzaOrder} from '../src/calculator'
+import {Calculator} from '../src/calculator'
 
 // http://osherove.com/tdd-kata-1/
 suite('TDD Kata Calculator tests', function () {
 
-    suite('suite', function () {
+    let calculator = new Calculator();
 
-        test('test', function () {
+    suite('when add empty string', function () {
+        test('returns 0', function () {
+            let expected = 0;
 
-            assert.equal(true, true);
+            let res = calculator.add('');
+
+            assert.equal(res, expected);
         });
+    });
 
+    suite('when add 1', function () {
+        test('returns 1', function () {
+            let expected = 1;
+
+            let res = calculator.add('1');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add 1,2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add('1,2');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add 1\n2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add('1\n2');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add 1\n2,3', function () {
+        test('returns 6', function () {
+            let expected = 6;
+
+            let res = calculator.add('1\n2,3');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add //;\n1;2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add('//;\n1;2');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add //;\n1;2\n1', function () {
+        test('returns 4', function () {
+            let expected = 4;
+
+            let res = calculator.add('//;\n1;2\n1');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add -1', function () {
+        test('exception negatives not allowed -1', function () {
+            assert.throws(() => calculator.add('-1'), /negatives not allowed -1/);
+        });
+    });
+
+    suite('when add -1,1,-2', function () {
+        test('exception negatives not allowed -1negatives not allowed -2', function () {
+            assert.throws(() => calculator.add('-1,1,-2'), /negatives not allowed -1negatives not allowed -2/);
+        });
+    });
+
+    suite('when add 1,1002', function () {
+        test('returns 1', function () {
+            let expected = 1;
+
+            let res = calculator.add('1,1002');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add //__\n1__2', function () {
+        test('returns 3', function () {
+            let expected = 3;
+
+            let res = calculator.add('//__\n1__2');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add //[,][;]\n1,2;3', function () {
+        test('returns 6', function () {
+            let expected = 6;
+
+            let res = calculator.add('//[,][;]\n1,2;3');
+
+            assert.equal(res, expected);
+        });
+    });
+
+    suite('when add //[,,,][;;]\n1,,,2;;3', function () {
+        test('returns 6', function () {
+            let expected = 6;
+
+            let res = calculator.add('//[,,,][;;]\n1,,,2;;3');
+
+            assert.equal(res, expected);
+        });
     });
 
 });
